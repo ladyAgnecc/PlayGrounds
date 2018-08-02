42b740d1b62bdcf3133ef9f70a46919462c8c4e4
Jenny Rykun <>
2018-08-02 15:49:01 +0300

    Lesson 1, Home work

---
+++ MyPlayground_L1_homeWork.playground/Contents.swift
@@ -0,0 +1,68 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+typealias Age = Int
+
+
+// task 2
+let a: String = ""
+
+
+// task 3
+var b: Int = 10
+
+
+// task 4
+var c: String = "Hello"
+let d: String = "World!"
+c = c + " " + d
+
+
+// task 6
+let e: Float = 11.8
+let f: Double = Double(e)
+
+
+// task 7
+let g: Int = 28
+print("Time is \(g) days")
+
+
+// task 8
+var h: Age = 50
+let j: Int = 5
+h = h + j
+
+
+// task 9
+var k: Int? = 5
+let m: Int = k!
+
+
+// task 10
+var n: Int? = 10
+let o: Int
+
+if let o = n {
+    print(o)
+}
+
+
+// task 11
+
+let road: Float = 100
+let speed: Float = 15
+let time: Float = 2.5
+let partOfTheWay: Float = speed * time
+let wayToFinish: Float = road - partOfTheWay
+
+
+
+
+
+
+
+
+
+
